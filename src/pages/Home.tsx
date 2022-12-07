77e84a2503cb7f383b2f045a41cdc3dc12d10636
Joel Elizaga <joel@elizaga.org>
2022-12-07 11:36:15 -0800

    Using our fetched data - More on state management #5

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,34 @@
-import { Component } from 'solid-js';
+import { Component, createEffect } from 'solid-js';
+import { repos, setUsername, username } from '../App';
 
 const Home: Component = () => {
+  const refetchWithUsername = (event: Event) => {
+    // Prevent reloading the page on click of submit button.
+    event.preventDefault();
+    // Get the `HTMLInputElement` in `#usernameInput`.
+    const usernameInput = document.querySelector("#usernameInput") as HTMLInputElement;
+    // Get the value of `usernameInput` (should be the value in 
+    // `#usernameInput`.)
+    setUsername(usernameInput.value);
+  }
+
+  // Waits for the data of `Home` to change before triggering, thanks to
+  // `createEffect()`.
+  createEffect(() => {
+    console.log(repos());
+  })
+
   return (
     <div>
-      <h2>Home</h2>
+      <form class="mb-3" onSubmit={(event) => refetchWithUsername(event) }>
+        <input type="text" class="p-1 align-middle" id="usernameInput" required />
+        <button class="btn btn-dark ms-3 w-auto">
+          Fetch
+        </button>
+      </form>
+      <h3>
+        GitHub repos for {username()}
+      </h3>
     </div>
   )
 }
