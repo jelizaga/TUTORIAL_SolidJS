18c087110d9a0d8212b1c96cf9c3b9018ef613ac
Joel Elizaga <joel@elizaga.org>
2022-12-07 12:05:09 -0800

    Adding functionality for saving repositories #8

--- src/pages/SavedRepos.tsx
+++ src/pages/SavedRepos.tsx
@@ -1,11 +1,18 @@
-import { Component } from 'solid-js';
+import { Component, createSignal, For } from 'solid-js';
+import RepoCard, { Repo } from '../components/RepoCard';
+
+const [savedRepos, setSavedRepos] = createSignal([]);
 
 const SavedRepos: Component = () => {
   return (
     <div>
-      <h2>SavedRepos</h2>
+      <h2>Your saved repos</h2>
+      <For each={savedRepos()}>
+        {(repo: Repo) => <RepoCard repo={repo} />}
+      </For>
     </div>
   )
 }
 
+export { setSavedRepos };
 export default SavedRepos;
