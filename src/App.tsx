a7d9e028e51330008c469bd63f745696f33fa4f3
Joel Elizaga <joel@elizaga.org>
2022-12-07 09:58:08 -0800

    Creating components in Solid.js #1

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import type { Component } from 'solid-js';
+import Nav from './components/Nav';
 
 const App: Component = () => {
   return (
     // We use `class` instead of the `className` used in React.
     <div>
+      <Nav />
       <h1>App component</h1>
     </div>
   );
