ef642e2f87a36f05e794b4a60cebf002e4bb552e
Joel Elizaga <joel@elizaga.org>
2022-12-07 11:12:32 -0800

    Data fetching and state management in Solid.js #4

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
 import { Route, Routes } from 'solid-app-router';
-import type { Component } from 'solid-js';
+import { Component, createEffect, createSignal } from 'solid-js';
 import Nav from "./components/Nav";
 import Home from './pages/Home';
 import SavedRepos from './pages/SavedRepos';
 
+// Set default state of `username`.
+// `createSignal` is relatively equivalent to React's `useState`.
+const [username, setUsername] = createSignal("jelizaga");
+const [repos, setRepos] = createSignal([]);
+
 const App: Component = () => {
+  // `createEffect` is relatively equivalent to React's `useEffect`.
+  createEffect(async () =>{
+    const res = await fetch(`https://api.github.com/users/${username()}/repos?sort=created`);
+    setRepos(await res.json());
+  });
   return (
     // We use `class` instead of the `className` used in React.
     <div class="container">
@@ -18,4 +28,6 @@ const App: Component = () => {
   );
 };
 
+// We can export `username` & `setUsername` for anywhere else in our app.
+export { username, setUsername, repos };
 export default App;
