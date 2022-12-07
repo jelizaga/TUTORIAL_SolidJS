724c35e2c24723494cf4258f0fa649f5558b940c
Joel Elizaga <joel@elizaga.org>
2022-12-07 10:48:52 -0800

    Setting up routing for our GitHub repos Solid.js app #2

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
+import { Route, Routes } from 'solid-app-router';
 import type { Component } from 'solid-js';
-import Nav from './components/Nav';
+import Nav from "./components/Nav";
+import Home from './pages/Home';
+import SavedRepos from './pages/SavedRepos';
 
 const App: Component = () => {
   return (
     // We use `class` instead of the `className` used in React.
-    <div>
+    <div class="container">
       <Nav />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/savedrepos" element={<SavedRepos />} />
+      </Routes>
       <h1>App component</h1>
     </div>
   );
--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,15 @@
 /* @refresh reload */
 import { render } from 'solid-js/web';
-
 import App from './App';
+import { Router } from 'solid-app-router';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-render(() => <App />, document.getElementById('root') as HTMLElement);
+render(() => 
+  (
+    // Wrapping `<App />` in `<Router>` allows us to use solid-app-router.
+    <Router>
+      <App />
+    </Router>
+  ),
+  document.getElementById('root') as HTMLElement
+);
